c3dfd08c64e9d2f4b16ab128f74669f2792ed3ee
Steve Gehrman <sgehrman@gmail.com>
2018-01-28 00:22:23 -0800

    sdf

--- src/StellarAPI.js
+++ src/StellarAPI.js
@@ -376,15 +376,22 @@ export default class StellarAPI {
   }
 
   buggy() {
-    const asset = new StellarSdk.Asset('LMB', 'GCYQSB3UQDSISB5LKAL2OEVLAYJNIR7LFVYDNKRMLWQKDCBX4PU3Z6JP')
-    try {
-      console.log(JSON.stringify(asset))
-      const xdr = asset.toXDRObject()
-      console.log(JSON.stringify(xdr))
-      const newAsset = StellarSdk.Asset.fromOperation(xdr)
-      console.log(JSON.stringify(newAsset))
-    } catch (error) {
-      console.log(error)
-    }
-  }
+    const promise = new Promise((resolve, reject) => {
+        const asset = new StellarSdk.Asset('LMB', 'GCYQSB3UQDSISB5LKAL2OEVLAYJNIR7LFVYDNKRMLWQKDCBX4PU3Z6JP')
+        try {
+          console.log(JSON.stringify(asset))
+          const xdr = asset.toXDRObject()
+          console.log(JSON.stringify(xdr))
+          const newAsset = StellarSdk.Asset.fromOperation(xdr)
+          console.log(JSON.stringify(newAsset))
+
+          resolve('worked')
+        } catch (error) {
+          console.log(error)
+          reject(error)
+        }
+      }
+    })
+
+  return promise
 }
