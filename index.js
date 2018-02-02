fad356e3757b786396c4808820129e940f6f0263
Steve Gehrman <sgehrman@gmail.com>
2018-02-01 19:52:20 -0800

    sdf

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import StellarAPIServer from './src/js/StellarAPIServer.js'
 import DonateDialog from './src/vue/DonateDialog.vue'
 import DonateView from './src/vue/DonateView.vue'
 import BuyTokenDialog from './src/vue/BuyTokenDialog.vue'
+import DialogTitleBar from './src/vue/DialogTitleBar.vue'
 
 export {
   StellarAPI,
   StellarAPIServer,
   DonateDialog,
   DonateView,
+  DialogTitleBar,
   BuyTokenDialog
 }
