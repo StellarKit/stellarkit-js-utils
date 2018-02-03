3260967b890231a23c71eed17a63ada50b5649eb
Steve Gehrman <sgehrman@gmail.com>
2018-02-03 04:47:11 -0800

    sdf

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,6 @@ module.exports = {
     // without this, we'll get two copies of jquery and triggers will fail
     // also package will be huge
     'jquery',
-    'stellar-sdk',
     'stellar-ledger-api'
   ],
   module: {
