dfdda1666381e4f9629eaf12c3b174632b28b2bc
Steve Gehrman <sgehrman@gmail.com>
2018-02-15 04:50:50 -0800

    debugging

--- webpack.config.js
+++ webpack.config.js
@@ -39,13 +39,13 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      output: {
-        comments: false
-      }
-    })
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   },
+    //   output: {
+    //     comments: false
+    //   }
+    // })
   ]
 }
