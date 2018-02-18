139d667052363c6d8c7ac7c383625509b2dc933f
Steve Gehrman <sgehrman@gmail.com>
2018-02-18 03:44:25 -0800

    sdf

--- src/js/LedgerAPI.js
+++ src/js/LedgerAPI.js
@@ -13,6 +13,7 @@ export default class LedgerAPI {
     return LedgerAPITransport.create()
       .then((t) => {
         this.transport = t
+        this.transport.setDebugMode(true)
         this.str = new StellarApp(this.transport)
       })
       .catch((error) => {
