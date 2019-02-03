73e17d6f0571d29b65c6018f300a8df2955ef9e3
Steve Gehrman <sgehrman@gmail.com>
2019-02-03 14:23:59 -0800

    removed stellar-sdk from external

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     libraryTarget: 'umd'
   },
   externals: [
-    "jquery", "stellar-sdk", "axios", "vue", "vuetify"
+    "jquery", "axios", "vue", "vuetify"
   ],
   // added to kill all comments, remove if you don't care (16k smaller too)
   optimization: {
